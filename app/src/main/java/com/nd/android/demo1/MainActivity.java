929b229221645cbb0f6d641ab6df6f991efcf9da
WangQingqing <1103809343@example.com>
2018-12-25 14:13:32 +0800

    Demo-2: horizontal screen

--- app/src/main/java/com/nd/android/demo1/MainActivity.java
+++ app/src/main/java/com/nd/android/demo1/MainActivity.java
@@ -1,13 +1,30 @@
 package com.nd.android.demo1;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1=(Button)findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//设置屏幕横屏
+                //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//设置屏幕竖屏
+                Toast toast=Toast.makeText(MainActivity.this,"点击事件",Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
